fbcdca0155d8c109f1d6f2380656d109060faf36
Marlier Whitney <marlier.whitney@outlook.com>
2022-06-09 17:45:21 +0200

    création backend

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+
+ mongoose.connect('mongodb+srv://WhitneyM:KQ44Lwbylw073EuH@cluster0.xl8le.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  
+app.use(express.json());
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+  
+module.exports = app;
